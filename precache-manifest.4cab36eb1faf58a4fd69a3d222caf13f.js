83ef85021f2cd29659de5ec0c17b0b6abad83094
Marija Kovacevic <Marija.Kovacevic@endava.com>
2019-07-09 20:17:19 +0200

    Initial commit

---
+++ precache-manifest.4cab36eb1faf58a4fd69a3d222caf13f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee004d76b4073902a91cd7999f922be5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7348d7d4d3394b6fa70",
+    "url": "/static/css/main.886b1846.chunk.css"
+  },
+  {
+    "revision": "c99ee1aa8147ed5da76e",
+    "url": "/static/js/2.b7c34336.chunk.js"
+  },
+  {
+    "revision": "e7348d7d4d3394b6fa70",
+    "url": "/static/js/main.367b6c18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
